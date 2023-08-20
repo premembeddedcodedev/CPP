6a57025a48e36393e71c602c75cdbcb470597a3e
premv <prem441@gmail.com>
2023-08-20 14:04:00 +0530

    Makefile and questionaire added in 4.Q

---
+++ Basics/4.Numberofoccurrence.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+//https://practice.geeksforgeeks.org/explore?page=1&category[]=Arrays&sortBy=submissions
+
+using namespace std;
+
+class solution {
+	public:
+		int count(int arr[], int n, int x) {
+					
+		}
+};
+
+int main()
+{
+	int t;
+
+	cin >> t;
+
+	while(t-->=0) {
+		int n, x;
+		cin >> n >> x;
+
+		int arr[n];
+
+		for(int i=0; i<n; i++) {
+			cin >> arr[i];
+		}
+
+		solution ob;
+	
+		auto ans = ob.count(arr, n, x);
+		cout << ans << "\n";
+	}
+
+	return 0;
+}
