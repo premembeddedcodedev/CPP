7e969ef3dfc806a0ffa6f569713d889592cdc6a1
premv <prem441@gmail.com>
2023-08-20 13:10:18 +0530

    Basic

---
+++ Basics/2.main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class my_class{
+	public:
+		my_class(){
+			cout << "Hello World \n";
+		}
+};
+
+// If you declare the variable then we will get print
+//my_class my_obj;
+
+int main() { 
+	my_class my_obj;
+
+}
