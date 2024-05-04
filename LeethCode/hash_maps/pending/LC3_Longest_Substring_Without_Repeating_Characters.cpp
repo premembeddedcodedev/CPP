3aa763e3886c2c943affe21b8c6ebdb295486fe3
Praveen Vuppala <prem441@gmail.com>
2024-05-04 20:38:56 +0530

    fixed the issue

--- LeethCode/hash_maps/pending/LC3_Longest_Substring_Without_Repeating_Characters.cpp
+++ LeethCode/hash_maps/pending/LC3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -10,8 +10,14 @@ int lengthOfLongestSubstring(string s)
 	int l = 0, r = 0, maxlen = INT_MIN;
 	map<int, int> mp;
 
-	cout << s.size();
-
+	cout << "string size : " << s.size() << "\n";
+	
+	// map default is 0	
+	cout << "string : " << mp[s[0]] << "\n";
+	for(int it = 0;it != 256; ++it) {
+		mp[it] = -1;
+	}
+	
 	for(r=0; r<s.size(); r++) {
 		
 		if(mp[s[r]] != -1 && (mp[s[r]] >= l))
@@ -28,7 +34,7 @@ int lengthOfLongestSubstring(string s)
 
 int main()
 {
-	string str = "abcabcbb";
+	string str = "abcabcbb"; //"bbbbb";
 
 	cout << "maxlen : " <<  lengthOfLongestSubstring(str) << "\n";
 
